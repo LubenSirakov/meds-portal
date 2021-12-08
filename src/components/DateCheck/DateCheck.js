8a64beb74289fbfdbce969ea8fd67f338aee3eda
Luben Sirakov <lubo95sirakov@gmail.com>
2021-12-08 19:07:16 +0200

    fixed  addDate function

--- src/components/DateCheck/DateCheck.js
+++ src/components/DateCheck/DateCheck.js
@@ -3,33 +3,31 @@ import './DateCheck.css';
 
 function DateCheck() {
 
-    
-
     const checkDate = (e) => {
         e.preventDefault();
 
         let formData = new FormData(e.currentTarget);
         let startDate = formData.get('start-date');
-        let daysToAdd = formData.get('days-to-add');
-
-
+        let daysToAdd = Number(formData.get('days-to-add'));
+        
         const addDate = (date, days) => {
+            
             let result = new Date(date);
-    
-            result.setDate(result.getDate() + days);
-    
+            result.setUTCDate(result.getDate() + days);
+
             let day = result.getDate();
+            
             let month = result.getMonth();
             month = month + 1;
+            
             let year = result.getFullYear();
-    
+
             let nextDate = `${day}-${month}-${year}`;
             console.log(nextDate);
         }
 
-        let newDate = addDate(startDate, daysToAdd);
+        addDate(startDate, daysToAdd);
 
-        // console.log(newDate);
         // let result = dateCalculator(startDate, daysToAdd);
 
     }
@@ -37,14 +35,18 @@ function DateCheck() {
     return (
         <form className="date-check-wrapper" onSubmit={checkDate}>
             <h2>Check when to get your next perscription</h2>
+
             <div className="mb-3">
-                <label for="input-date" className="form-label">Input date</label>
-                <input type="text" className="form-control" name="start-date" id="input-date" />
+                <label for="input-date" className="form-label">Input date</label>           
+                <input id="party" type="datetime-local" name="start-date"/>
+
             </div>
+
             <div className="mb-3">
                 <label for="input-days" className="form-label">Days to add</label>
-                <input type="text" className="form-control" name="days-to-add" id="input-days" />
+                <input type="number" className="form-control" name="days-to-add" id="input-days" />
             </div>
+
             <input className="button submit" type="submit" value="Check date" />
         </form>
     );
